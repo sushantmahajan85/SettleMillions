d0e61d10bfc9c576e35ae864d8a5e7dd4668b1c1
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-04-15 00:22:16 +0530

    fjehfej

--- public/js/fbSignup.js
+++ public/js/fbSignup.js
@@ -1,63 +1,63 @@
 const passvaluefb = async (name, email, password, passwordConfirm) => {
    try {
-       const result = await axios({
-           method: 'POST',
-           url: '/api/v1/users/signup',
-           data: {
-               email,
-               name,
-               password,
-               passwordConfirm
-           }
-       });
-       if (result.data.status === 'success') {
-           alert('sign up successful');
-           window.setTimeout(() => {
-               location.assign('/');
-           }, 1000);
-       }
+      const result = await axios({
+         method: 'POST',
+         url: '/api/v1/users/signup',
+         data: {
+            email,
+            name,
+            password,
+            passwordConfirm
+         }
+      });
+      if (result.data.status === 'success') {
+         alert('sign up successful');
+         window.setTimeout(() => {
+            location.assign('/');
+         }, 1000);
+      }
 
    } catch (err) {
-       console.log(err);
+      console.log(err);
    }
 };
 
-window.fbAsyncInit = function() {
+window.fbAsyncInit = function () {
    FB.init({
-       appId      : '1321461864731329',
-       cookie     : true,
-       xfbml      : true,
-       version    : 'v6.0'
+      appId: '1321461864731329',
+      cookie: true,
+      xfbml: true,
+      version: 'v6.0'
    });
-   
-   FB.getLoginStatus(function(response) {
+
+   FB.getLoginStatus(function (response) {
       statusChangeCallback(response);
    });
 
-   FB.AppEvents.logPageView();  
-       
-   };
+   FB.AppEvents.logPageView();
 
-   (function(d, s, id){
+};
+
+(function (d, s, id) {
    var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
+   if (d.getElementById(id)) { return; }
    js = d.createElement(s); js.id = id;
    js.src = "https://connect.facebook.net/en_US/sdk.js";
    fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+}(document, 'script', 'facebook-jssdk'));
 
 
 
-function checkLoginState() {
-   FB.getLoginStatus(function(response) {
-       statusChangeCallback(response);
-   });
-}
+// function checkLoginState() {
+//    FB.getLoginStatus(function(response) {
+//        statusChangeCallback(response);
+//    });
+// }
 
 function statusChangeCallback(response) {
-   if(response.status === 'connected'){
+   if (response.status === 'connected') {
       console.log('Logged In');
-   }else{
+   } else {
       console.log('Not Logged In')
    }
 }
@@ -65,9 +65,9 @@ function statusChangeCallback(response) {
 let fbName;
 let fbEmail;
 
-function testAPI(){
-   FB.api('/me?fields=name,email', function(response){
-      if(response && !response.error){
+function testAPI() {
+   FB.api('/me?fields=name,email', function (response) {
+      if (response && !response.error) {
          console.log(response);
          fbName = response.name;
          fbEmail = response.email;
@@ -75,8 +75,8 @@ function testAPI(){
    });
 }
 
-   const name = fbName;
-   const email = fbEmail;
-   const password = 'password';
-   const passwordConfirm = 'password';
-   passvaluefb(name, email, password, passwordConfirm);
+const name = fbName;
+const email = fbEmail;
+const password = 'password';
+const passwordConfirm = 'password';
+passvaluefb(name, email, password, passwordConfirm);

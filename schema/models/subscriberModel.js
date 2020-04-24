2b6c3eb8452259f20de76d93e3fb49fd565b3476
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-04-24 15:06:07 +0530

    fjehfej

--- schema/models/subscriberModel.js
+++ schema/models/subscriberModel.js
@@ -1,35 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const subscriberSchema = new mongoose.Schema({
+const subscriberSchema = new mongoose.Schema(
+  {
     user: {
-        type: mongoose.Schema.ObjectId, ref: 'User',
-        required: [true, 'subscription Not Possible Without A User']
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "subscription Not Possible Without A User"],
     },
     subscribedUser: {
-        type: mongoose.Schema.ObjectId, ref: 'User',
-        required: [true, 'subscription Not Possible Without A User']
-    }
-}, { toJSON: { virtuals: true }, toObject: { virtuals: true } });
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "subscription Not Possible Without A User"],
+    },
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
 subscriberSchema.index({ user: 1, subscribedUser: 1 }, { unique: true });
 
-subscriberSchema.virtual('subscribedDeals', {
-    ref: 'Deal',
-    foreignField: 'user',
-    localField: 'subscribedUser'
+subscriberSchema.virtual("subscribedDeals", {
+  ref: "Deal",
+  foreignField: "user",
+  localField: "subscribedUser",
 });
 
-subscriberSchema.pre(/^find/, function (next) {
-    this.populate({
-        path: 'user',
-        // select: 'name'
-    }).populate({
-        path: 'subscribedUser',
-        // select: 'name'
-    });
+subscriberSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: "user",
+    // select: 'name'
+  }).populate({
+    path: "subscribedUser",
+    // select: 'name'
+  });
 
-    next();
+  next();
 });
 
-const Subscriber = mongoose.model('Subscriber', subscriberSchema);
+const Subscriber = mongoose.model("Subscriber", subscriberSchema);
 module.exports = Subscriber;

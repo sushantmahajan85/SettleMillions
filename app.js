94c5e37353ff6439ad45b565de59c78ec1a02d97
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-04-24 21:15:06 +0530

    fjehfej

--- app.js
+++ app.js
@@ -18,46 +18,16 @@ app.use(express.json());
 
 let segment_one; // Get the last part of the array (-1)
 let segment_two; // Get the last part of the array (-1)
-let segment_three; 
+let segment_three;
 
 const limiter = rateLimit({
   max: 3,
   windowMs: 60 * 60 * 1000,
   message: "too many request from this ip..try again in an hour",
 });
-<<<<<<< HEAD
 app.use("/deal", limiter);
 app.use("/api/v1/users", limiter);
 app.use("/api/v1/deals", limiter);
-=======
-app.use("/deal", function(req, res, next) {
-  const full_url = req.url;
-  // var full_url = document.URL; // Get current url
-  var url_array = full_url.split("/"); // Split the string into an array with / as separator
-  segment_one = url_array[url_array.length - 3]; // Get the last part of the array (-1)
-  segment_two = url_array[url_array.length - 2]; // Get the last part of the array (-1)
-  segment_three = url_array[url_array.length - 1];
-  console.log(segment_one);
-  console.log(segment_two);
-  console.log(segment_three);
-  console.log(`/deal/${segment_one}/${segment_two}/${segment_three}`)
-  next();
-});
-
-app.use(`/deal/${segment_one}/${segment_two}/${segment_three}`, limiter);
-
-
-// app.use("/deal/:dealId", limiter, function(req, res, next) {
-//   const full_url = req.url;
-
-//   // var full_url = document.URL; // Get current url
-//   var url_array = full_url.split("/"); // Split the string into an array with / as separator
-//   var last_segment = url_array[url_array.length - 2]; // Get the last part of the array (-1)
-//   console.log(last_segment);
-
-//   next();
-// });
->>>>>>> bb2ceb65c6acb2a26eb67ae0b83c609042df7739
 app.use(compression());
 
 app.set("view engine", "ejs");

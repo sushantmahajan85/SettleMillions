ed1faea97dde7a8c68778d0a4ea3e9a1179767a5
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-04-24 19:23:53 +0530

    fjehfej

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const viewController = require('../controllers/viewController');
-const authController = require('../controllers/authController');
+const express = require("express");
+const viewController = require("../controllers/viewController");
+const authController = require("../controllers/authController");
 const router = express.Router();
-router.get('/login', viewController.getLoginForm);
-router.get('/signup', viewController.getSignupForm);
-router.get('/verification', viewController.getVerificationForm);
-router.get('/recruitments', authController.protect, viewController.getRecruitmentsData);
-router.get('/', viewController.mainPage);
-router.get('/member/:id', viewController.getMemberData);
-router.get('/deal/:dealId/postedBy/:sellerId', viewController.dealPage);
-router.get('/likedDeals', authController.protect, viewController.getLikedDeals);
-router.get('/subscriptions', authController.protect, viewController.getSubscriptions);
-router.get('/newDeal', viewController.createNewDeal);
-router.get('/recent', viewController.recently);
-router.get('/updateUserSettings', viewController.updateUserSettings);
-
+router.get("/login", viewController.getLoginForm);
+router.get("/signup", viewController.getSignupForm);
+router.get("/verification", viewController.getVerificationForm);
+router.get(
+  "/recruitments",
+  authController.protect,
+  viewController.getRecruitmentsData
+);
+router.get("/", viewController.mainPage);
+router.get("/member/:id", viewController.getMemberData);
+router.get(
+  "/deal/:dealId/postedBy/:sellerId",
+  authController.limiter,
+  viewController.dealPage
+);
+router.get("/likedDeals", authController.protect, viewController.getLikedDeals);
+router.get(
+  "/subscriptions",
+  authController.protect,
+  viewController.getSubscriptions
+);
+router.get("/newDeal", viewController.createNewDeal);
+router.get("/recent", viewController.recently);
+router.get("/updateUserSettings", viewController.updateUserSettings);
 
 module.exports = router;

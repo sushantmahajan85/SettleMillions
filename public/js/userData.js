1fafe3002aaaba1880260001bc4d699cf29d2fac
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-04-25 18:00:14 +0530

    fjehfej

---
+++ public/js/userData.js
@@ -0,0 +1,28 @@
+const passvalue = async (email, password) => {
+  try {
+    const result = await axios({
+      method: "POST",
+      url: "/api/v1/users/login",
+      data: {
+        email,
+        password,
+      },
+    });
+    if (result.data.status === "success") {
+      alert("login successful");
+      window.setTimeout(() => {
+        location.assign("/subscriptions");
+      }, 1000);
+    }
+    // console.log(result);
+  } catch (err) {
+    console.log(err.response.data);
+  }
+};
+
+document.getElementById("login100-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  passvalue(email, password);
+});

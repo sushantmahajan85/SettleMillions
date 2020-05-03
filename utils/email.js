550da0442d02ee8c4fe053165119f527f000ff13
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-05-03 20:26:59 +0530

    fjehfej

--- utils/email.js
+++ utils/email.js
@@ -18,6 +18,8 @@ module.exports = class Email {
     // }
     return nodemailer.createTransport({
       service: "SendGrid",
+      port: process.env.EMAIL_PORT,
+      host: process.env.EMAIL_HOST,
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD,

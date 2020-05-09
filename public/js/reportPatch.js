a6ccf446d8e3716435c93f726f94429441abec0c
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-05-10 00:09:58 +0530

    fjehfej

---
+++ public/js/reportPatch.js
@@ -0,0 +1,41 @@
+// console.log('hey there');
+//const Deal = require('./../../schema/models/dealModel');
+
+const passvalueR = async function(dealId, report) {
+  console.log("hey there");
+
+  try {
+    const result = await axios({
+      method: "PATCH",
+      url: `/api/v1/deals/${dealId}`,
+      data: {
+        reportCount: { $inc: 1 },
+      },
+    });
+    if (result.data.status === "success") {
+      alert("reported");
+      console.log("reported");
+      // window.setTimeout(() => {
+      //     location.assign('/');
+      // }, 1000);
+      //el.form.submit();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// var url = 'http://www.site.com/234234234';
+// var id = url.substring(url.lastIndexOf('/') + 1);
+// alert(id);
+// document.getElementById('report_btn').addEventListener('click', e => {
+//     e.preventDefault();
+
+//     // const dealName = document.getElementById('dealName').value;
+//     // const affiliateLink = document.getElementById('affiliateLink').value;
+//     // const category = document.getElementById('category').value;
+//     // const company = document.getElementById('company').value;
+//     // const mrp = document.getElementById('mrp').value;
+//     // const dealPrice = document.getElementById('dealPrice').value;
+//     passvalueR();
+// })

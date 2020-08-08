8387450572dd5f904d33a4582def19c6696ade9e
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-08-08 23:28:19 +0530

    fjehfej

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -312,126 +312,125 @@ exports.mainPage = catchAsync(async (req, res) => {
   let recentlyViewed = [];
   const user = await User.findById(req.logged);
 
-if (req.logged) {
-  if (req.cookies.one !== undefined) {
-    user.cookies[0] = req.cookies.one.id;
-
-    recentlyViewed[cooCount] = req.cookies.one;
-    cooCount++;
-    user.r1 =
-      req.cookies.one.dealName +
-      " " +
-      req.cookies.one.titleDis +
-      " " +
-      req.cookies.one.owner +
-      " " +
-      req.cookies.one.company +
-      " " +
-      req.cookies.one.category +
-      " " +
-      req.cookies.one.user;
-    if (req.cookies.one.tags) {
-      for (var i = 0; i < Object.keys(req.cookies.one.tags).length; i++) {
-        user.r1 = user.r1 + " " + req.cookies.one.tags[i];
-      }
-    }
-  }
-  //console.log(Object.keys(req.cookies.one.tags).length);
-  if (req.cookies.two !== undefined) {
-    user.cookies[1] = req.cookies.two.id;
-    recentlyViewed[cooCount] = req.cookies.two;
-    cooCount++;
-    user.r2 =
-      req.cookies.two.dealName +
-      " " +
-      req.cookies.two.titleDis +
-      " " +
-      req.cookies.two.owner +
-      " " +
-      req.cookies.two.company +
-      " " +
-      req.cookies.two.category +
-      " " +
-      req.cookies.two.user;
-    if (req.cookies.two.tags) {
-      for (var i = 0; i < Object.keys(req.cookies.two.tags).length; i++) {
-        user.r2 = user.r2 + " " + req.cookies.two.tags[i];
-      }
-    }
-  }
-  if (req.cookies.three !== undefined) {
-    user.cookies[2] = req.cookies.three.id;
-    recentlyViewed[cooCount] = req.cookies.three;
-    cooCount++;
-    user.r3 =
-      req.cookies.three.dealName +
-      " " +
-      req.cookies.three.titleDis +
-      " " +
-      req.cookies.three.owner +
-      " " +
-      req.cookies.three.company +
-      " " +
-      req.cookies.three.category +
-      " " +
-      req.cookies.three.user;
-    if (req.cookies.three.tags) {
-      for (var i = 0; i < Object.keys(req.cookies.three.tags).length; i++) {
-        user.r3 = user.r3 + " " + req.cookies.three.tags[i];
-      }
-    }
-  }
-  if (req.cookies.four !== undefined) {
-    user.cookies[3] = req.cookies.four.id;
-    recentlyViewed[cooCount] = req.cookies.four;
-    cooCount++;
-    user.r4 =
-      req.cookies.four.dealName +
-      " " +
-      req.cookies.four.titleDis +
-      " " +
-      req.cookies.four.owner +
-      " " +
-      req.cookies.four.company +
-      " " +
-      req.cookies.four.category +
-      " " +
-      req.cookies.four.user;
-    if (req.cookies.four.tags) {
-      for (var i = 0; i < Object.keys(req.cookies.four.tags).length; i++) {
-        user.r4 = user.r4 + " " + req.cookies.four.tags[i];
-      }
-    }
-  }
-  if (req.cookies.five !== undefined) {
-    user.cookies[4] = req.cookies.five.id;
-    recentlyViewed[cooCount] = req.cookies.five;
-    cooCount++;
-    user.r5 =
-      req.cookies.five.dealName +
-      " " +
-      req.cookies.five.titleDis +
-      " " +
-      req.cookies.five.owner +
-      " " +
-      req.cookies.five.company +
-      " " +
-      req.cookies.five.category +
-      " " +
-      req.cookies.five.user;
-    if (req.cookies.five.tags) {
-      for (var i = 0; i < Object.keys(req.cookies.five.tags).length; i++) {
-        user.r5 = user.r5 + " " + req.cookies.five.tags[i];
-      }
-    }
-  }
-  
-  user.save();
-  rec = user.r1 + " " + user.r2 + " " + user.r3 + " " + user.r4 +" " + user.r5;
-  
-}else{
+  if (req.logged) {
     if (req.cookies.one !== undefined) {
-  
+      user.cookies[0] = req.cookies.one.id;
+
+      recentlyViewed[cooCount] = req.cookies.one;
+      cooCount++;
+      user.r1 =
+        req.cookies.one.dealName +
+        " " +
+        req.cookies.one.titleDis +
+        " " +
+        req.cookies.one.owner +
+        " " +
+        req.cookies.one.company +
+        " " +
+        req.cookies.one.category +
+        " " +
+        req.cookies.one.user;
+      if (req.cookies.one.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.one.tags).length; i++) {
+          user.r1 = user.r1 + " " + req.cookies.one.tags[i];
+        }
+      }
+    }
+    //console.log(Object.keys(req.cookies.one.tags).length);
+    if (req.cookies.two !== undefined) {
+      user.cookies[1] = req.cookies.two.id;
+      recentlyViewed[cooCount] = req.cookies.two;
+      cooCount++;
+      user.r2 =
+        req.cookies.two.dealName +
+        " " +
+        req.cookies.two.titleDis +
+        " " +
+        req.cookies.two.owner +
+        " " +
+        req.cookies.two.company +
+        " " +
+        req.cookies.two.category +
+        " " +
+        req.cookies.two.user;
+      if (req.cookies.two.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.two.tags).length; i++) {
+          user.r2 = user.r2 + " " + req.cookies.two.tags[i];
+        }
+      }
+    }
+    if (req.cookies.three !== undefined) {
+      user.cookies[2] = req.cookies.three.id;
+      recentlyViewed[cooCount] = req.cookies.three;
+      cooCount++;
+      user.r3 =
+        req.cookies.three.dealName +
+        " " +
+        req.cookies.three.titleDis +
+        " " +
+        req.cookies.three.owner +
+        " " +
+        req.cookies.three.company +
+        " " +
+        req.cookies.three.category +
+        " " +
+        req.cookies.three.user;
+      if (req.cookies.three.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.three.tags).length; i++) {
+          user.r3 = user.r3 + " " + req.cookies.three.tags[i];
+        }
+      }
+    }
+    if (req.cookies.four !== undefined) {
+      user.cookies[3] = req.cookies.four.id;
+      recentlyViewed[cooCount] = req.cookies.four;
+      cooCount++;
+      user.r4 =
+        req.cookies.four.dealName +
+        " " +
+        req.cookies.four.titleDis +
+        " " +
+        req.cookies.four.owner +
+        " " +
+        req.cookies.four.company +
+        " " +
+        req.cookies.four.category +
+        " " +
+        req.cookies.four.user;
+      if (req.cookies.four.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.four.tags).length; i++) {
+          user.r4 = user.r4 + " " + req.cookies.four.tags[i];
+        }
+      }
+    }
+    if (req.cookies.five !== undefined) {
+      user.cookies[4] = req.cookies.five.id;
+      recentlyViewed[cooCount] = req.cookies.five;
+      cooCount++;
+      user.r5 =
+        req.cookies.five.dealName +
+        " " +
+        req.cookies.five.titleDis +
+        " " +
+        req.cookies.five.owner +
+        " " +
+        req.cookies.five.company +
+        " " +
+        req.cookies.five.category +
+        " " +
+        req.cookies.five.user;
+      if (req.cookies.five.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.five.tags).length; i++) {
+          user.r5 = user.r5 + " " + req.cookies.five.tags[i];
+        }
+      }
+    }
+
+    user.save();
+    rec =
+      user.r1 + " " + user.r2 + " " + user.r3 + " " + user.r4 + " " + user.r5;
+  } else {
+    if (req.cookies.one !== undefined) {
       recentlyViewed[cooCount] = req.cookies.one;
       cooCount++;
       rec1 =
@@ -538,17 +537,15 @@ if (req.logged) {
       }
     }
 
-    rec = rec1 + " " + rec2 + " " + rec3 + " " + rec4 +" " + rec5;
-}
- 
+    rec = rec1 + " " + rec2 + " " + rec3 + " " + rec4 + " " + rec5;
+  }
 
   //console.log(rec);
   //const t = await Deal.find({ trendRatio: { $gte: 4 } });
-  
 
-  console.log(user);
+  // console.log(user);
 
-  console.log(rec);
+  // console.log(rec);
 
   const recommendedDeals = await Deal.find(
     { $text: { $search: rec } },
@@ -565,7 +562,7 @@ if (req.logged) {
   for (var i = 0; i < cooCount; i++) {
     recommendedDeals[i] = undefined;
   }
-  console.log(recommendedDeals);
+  // console.log(recommendedDeals);
   if (req.query.search || req.query.sort) {
     // await Deal.ensureIndexes({ dealName: 'text' });
 
@@ -673,8 +670,7 @@ if (req.logged) {
     //   // await Deal.findByIdAndUpdate({ _id: deal._id }, { trendRatio: tem });
     // }
 
-    
-    const topUsersToday = await User.find().sort([['rankLatest', -1]]);
+    const topUsersToday = await User.find().sort([["rankLatest", -1]]);
     // console.log(topUsersToday);
 
     // const user = await User.findById(req.user);
@@ -735,11 +731,249 @@ exports.getMemberData = catchAsync(async (req, res) => {
       await Deal.findOneAndDelete({ _id: joChahiye[0] });
     }
   }
+  if (req.logged) {
+    const user = await User.findById(req.logged);
+    if (req.cookies.one !== undefined) {
+      user.cookies[0] = req.cookies.one.id;
 
-  const deals = await Deal.find({ user: req.params.id });
+      // recentlyViewed[cooCount] = req.cookies.one;
+      // cooCount++;
+      user.r1 =
+        req.cookies.one.dealName +
+        " " +
+        req.cookies.one.titleDis +
+        " " +
+        req.cookies.one.owner +
+        " " +
+        req.cookies.one.company +
+        " " +
+        req.cookies.one.category +
+        " " +
+        req.cookies.one.user;
+      if (req.cookies.one.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.one.tags).length; i++) {
+          user.r1 = user.r1 + " " + req.cookies.one.tags[i];
+        }
+      }
+    }
+    //console.log(Object.keys(req.cookies.one.tags).length);
+    if (req.cookies.two !== undefined) {
+      user.cookies[1] = req.cookies.two.id;
+      // recentlyViewed[cooCount] = req.cookies.two;
+      // cooCount++;
+      user.r2 =
+        req.cookies.two.dealName +
+        " " +
+        req.cookies.two.titleDis +
+        " " +
+        req.cookies.two.owner +
+        " " +
+        req.cookies.two.company +
+        " " +
+        req.cookies.two.category +
+        " " +
+        req.cookies.two.user;
+      if (req.cookies.two.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.two.tags).length; i++) {
+          user.r2 = user.r2 + " " + req.cookies.two.tags[i];
+        }
+      }
+    }
+    if (req.cookies.three !== undefined) {
+      user.cookies[2] = req.cookies.three.id;
+      // recentlyViewed[cooCount] = req.cookies.three;
+      // cooCount++;
+      user.r3 =
+        req.cookies.three.dealName +
+        " " +
+        req.cookies.three.titleDis +
+        " " +
+        req.cookies.three.owner +
+        " " +
+        req.cookies.three.company +
+        " " +
+        req.cookies.three.category +
+        " " +
+        req.cookies.three.user;
+      if (req.cookies.three.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.three.tags).length; i++) {
+          user.r3 = user.r3 + " " + req.cookies.three.tags[i];
+        }
+      }
+    }
+    if (req.cookies.four !== undefined) {
+      user.cookies[3] = req.cookies.four.id;
+      // recentlyViewed[cooCount] = req.cookies.four;
+      // cooCount++;
+      user.r4 =
+        req.cookies.four.dealName +
+        " " +
+        req.cookies.four.titleDis +
+        " " +
+        req.cookies.four.owner +
+        " " +
+        req.cookies.four.company +
+        " " +
+        req.cookies.four.category +
+        " " +
+        req.cookies.four.user;
+      if (req.cookies.four.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.four.tags).length; i++) {
+          user.r4 = user.r4 + " " + req.cookies.four.tags[i];
+        }
+      }
+    }
+    if (req.cookies.five !== undefined) {
+      user.cookies[4] = req.cookies.five.id;
+      // recentlyViewed[cooCount] = req.cookies.five;
+      // cooCount++;
+      user.r5 =
+        req.cookies.five.dealName +
+        " " +
+        req.cookies.five.titleDis +
+        " " +
+        req.cookies.five.owner +
+        " " +
+        req.cookies.five.company +
+        " " +
+        req.cookies.five.category +
+        " " +
+        req.cookies.five.user;
+      if (req.cookies.five.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.five.tags).length; i++) {
+          user.r5 = user.r5 + " " + req.cookies.five.tags[i];
+        }
+      }
+    }
 
+    user.save();
+    rec =
+      user.r1 + " " + user.r2 + " " + user.r3 + " " + user.r4 + " " + user.r5;
+  } else {
+    if (req.cookies.one !== undefined) {
+      // recentlyViewed[cooCount] = req.cookies.one;
+      // cooCount++;
+      rec1 =
+        req.cookies.one.dealName +
+        " " +
+        req.cookies.one.titleDis +
+        " " +
+        req.cookies.one.owner +
+        " " +
+        req.cookies.one.company +
+        " " +
+        req.cookies.one.category +
+        " " +
+        req.cookies.one.user;
+      if (req.cookies.one.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.one.tags).length; i++) {
+          rec1 = rec1 + " " + req.cookies.one.tags[i];
+        }
+      }
+    }
+    //console.log(Object.keys(req.cookies.one.tags).length);
+    if (req.cookies.two !== undefined) {
+      // recentlyViewed[cooCount] = req.cookies.two;
+      // cooCount++;
+      rec2 =
+        req.cookies.two.dealName +
+        " " +
+        req.cookies.two.titleDis +
+        " " +
+        req.cookies.two.owner +
+        " " +
+        req.cookies.two.company +
+        " " +
+        req.cookies.two.category +
+        " " +
+        req.cookies.two.user;
+      if (req.cookies.two.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.two.tags).length; i++) {
+          rec2 = rec2 + " " + req.cookies.two.tags[i];
+        }
+      }
+    }
+    if (req.cookies.three !== undefined) {
+      // recentlyViewed[cooCount] = req.cookies.three;
+      // cooCount++;
+      rec3 =
+        req.cookies.three.dealName +
+        " " +
+        req.cookies.three.titleDis +
+        " " +
+        req.cookies.three.owner +
+        " " +
+        req.cookies.three.company +
+        " " +
+        req.cookies.three.category +
+        " " +
+        req.cookies.three.user;
+      if (req.cookies.three.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.three.tags).length; i++) {
+          rec3 = rec3 + " " + req.cookies.three.tags[i];
+        }
+      }
+    }
+    if (req.cookies.four !== undefined) {
+      // recentlyViewed[cooCount] = req.cookies.four;
+      // cooCount++;
+      rec4 =
+        req.cookies.four.dealName +
+        " " +
+        req.cookies.four.titleDis +
+        " " +
+        req.cookies.four.owner +
+        " " +
+        req.cookies.four.company +
+        " " +
+        req.cookies.four.category +
+        " " +
+        req.cookies.four.user;
+      if (req.cookies.four.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.four.tags).length; i++) {
+          rec4 = rec4 + " " + req.cookies.four.tags[i];
+        }
+      }
+    }
+    if (req.cookies.five !== undefined) {
+      // recentlyViewed[cooCount] = req.cookies.five;
+      // cooCount++;
+      rec5 =
+        req.cookies.five.dealName +
+        " " +
+        req.cookies.five.titleDis +
+        " " +
+        req.cookies.five.owner +
+        " " +
+        req.cookies.five.company +
+        " " +
+        req.cookies.five.category +
+        " " +
+        req.cookies.five.user;
+      if (req.cookies.five.tags) {
+        for (var i = 0; i < Object.keys(req.cookies.five.tags).length; i++) {
+          rec5 = rec5 + " " + req.cookies.five.tags[i];
+        }
+      }
+    }
+
+    rec = rec1 + " " + rec2 + " " + rec3 + " " + rec4 + " " + rec5;
+  }
+
+  const dealTime = await Deal.find({ user: req.params.id });
+  console.log(rec);
+  const deals = await Deal.find(
+    { $text: { $search: rec } },
+    { score: { $meta: "textScore" } }
+
+    //{ trendRatio: { $gte: 4 } }
+  ).sort({ score: { $meta: "textScore" } });
+  const userId = req.params.id;
+  // console.log(recommendedDeals);
   res.status(200).render("members", {
     deals,
+    userId,
+    dealTime,
   });
 });
 

466d45c1bf7fa221cce0053845cc725fa323743c
namanjain323232 <namanjain24682468@gmail.com>
2020-12-20 20:03:31 +0530

    facebook

--- schema/models/dealModel.js
+++ schema/models/dealModel.js
@@ -24,7 +24,7 @@ const dealSchema = new mongoose.Schema(
     },
     short: {
       type: String,
-      default: shortid.generate(),
+      // default: shortid.generate(),
       //required: true,
       //unique: true
     },
@@ -46,7 +46,7 @@ type: String
     time: { type: Number, default: 0 },
     createdAt: {
       type: Number,
-      default : Date.now()
+      // default : Date.now()
     },
     titleDis: {
       type: String,
@@ -115,14 +115,11 @@ dealSchema.pre("save",async function(next){
 next();
 })
 dealSchema.post("save", async function() {
-<<<<<<< HEAD
+  this.discount = Math.round((((this.dealPrice - this.mrp)/(this.dealPrice))*100));
+  this.long = `127.0.0.1:4000/deal/${this._id}/postedBy/${this.user}`;
   const shorter = shortid.generate();
   this.createdAt = Date.now();
   this.short = shorter;
-=======
->>>>>>> e8e4aa902d1880b993b5221d3bc187c177215ba1
-  this.discount = Math.round((((this.dealPrice - this.mrp)/(this.dealPrice))*100));
-  this.long = `127.0.0.1:4000/deal/${this._id}/postedBy/${this.user}`;
   this.save();
 });
 const Deal = mongoose.model("Deal", dealSchema);

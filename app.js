722e48bd321678d003778a054f2a6a6fddee58a0
sushantmahajan85 <smahajan3_be18@thapar.edu>
2021-01-01 01:46:38 +0530

    server

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use(cookieParser());
 //     next();
 // });
 
-// app.use(globalErrorHandler);
+app.use(globalErrorHandler);
 
 app.use("/api/v1/report", reportRouter);
 app.use("/api/v1/users", userRouter);
@@ -106,7 +106,7 @@ app.use("/", viewRouter);
 app.all("*", (req, res, next) => {
   res.status(200).render('error');
 });
-// app.use(globalErrorHandler);
+app.use(globalErrorHandler);
 const port = process.env.PORT;
 app.listen(port || 4000, () => {
   console.log("Listening bro");
